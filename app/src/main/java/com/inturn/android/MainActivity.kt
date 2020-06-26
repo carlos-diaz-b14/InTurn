974a4ef46b4ae73576918052d1794f148aaab5cf
aaron chen <ixdezbtoeck@gmail.com>
2020-06-26 09:37:15 -0700

    firebase Read Write Test

--- app/src/main/java/com/inturn/android/MainActivity.kt
+++ app/src/main/java/com/inturn/android/MainActivity.kt
@@ -2,11 +2,28 @@ package com.inturn.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "KotlinActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        basicReadWrite()
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/inturn/android/Model/Restaurant.kt
@@ -0,0 +1,7 @@
+package com.inturn.android.Model
+
+data class Restaurant(
+    var address: String? = null,
+    var id : String? = null,
+    var name : String? = null
+)
